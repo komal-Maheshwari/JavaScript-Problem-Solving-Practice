c902000141c85e81b395d708aaa0c4bf7427d43a
komal-Maheshwari <83787447+komal-Maheshwari@users.noreply.github.com>
2021-05-26 21:08:06 +0500

    JS Coding Challenge-1 Done

---
+++ Topic-3 JS Operators/Operators.js
@@ -0,0 +1,24 @@
+// var x = 5, y = 10, z = 15;
+
+// x + y;  15
+// y - x; 5.
+// x * y; 50.
+// y / x; 2.
+// x % 2;  1.
+// x++;  6.
+// x--;  4.
+
+
+// var a = 5, b = 10, c = "5";
+
+// var x = a; 5.
+// a == c; 5.
+// a === c; true.
+// a == x; 5.
+// a != b;  false.
+// a > b; false.
+// a < b; true.
+// a >= b; false.
+// a <= b; true.
+// a >= c; true.
+// a <= c; true.
